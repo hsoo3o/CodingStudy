b167054a327402704027e00d18dc0cef4135998c
cococindy98 <cococindy98@naver.com>
2022-07-08 16:10:58 +0900

    4차시

--- 3차시/bj/4_12015.py
+++ 3차시/bj/4_12015.py
@@ -1,31 +1,22 @@
 import sys
-from collections import deque
 n = int(input())
 
-num = list(map(int, sys.stdin.readline().split(' ')))
-
-sortnum = sorted(num)
+anum = list(map(int, sys.stdin.readline().split(' ')))
+all_ans = []
 ans = 0
-mans = 0
-res = []
-p = 0
-i = 0
-lastindex = 0
-for i in num:
-    index = sortnum.index(i)
+res = [0]
+for j in range(n-1):
+    num = anum[j:]
+    ans = 0
+    res = [0]
+    for i in range(len(num)-1):
+        if num[i] <= num[i+1] and num[i] >= res[-1]:
+            ans += 1
+            res.append(num[i])
 
-    res.append(index+p)
-    sortnum[index] = 0
-    
-print(res)
-
-
-for i in range(n-1):
-    if res[i] > res[i+1] and ans >= mans:
-        mans = ans
-        ans = 0
-    else:
+    if num[-1] >= res[-1]:
         ans += 1
-print(mans)
-    
+    all_ans.append(ans)
+print(max(all_ans))
 
+4,7,10
