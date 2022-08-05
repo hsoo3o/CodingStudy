99969b7238915b0a1b05a2c753136f11a380d48f
hsoo3o <hyesoo030@gmail.com>
2022-08-05 16:32:40 +0900

    11"
    "

--- 11차시/4_14888.py
+++ 11차시/4_14888.py
@@ -1,32 +1,12 @@
-from collections import deque
 import sys
 def dfs(v,order):
-    global min_sum,max_sum,sum_
     if v == opnum:
-        order = iter(order)
-        for i in range(1,n):
-            o = next(order)
-            if o == 0:
-                sum_ += num[i]
-            elif o == 1:
-                sum_ -= num[i]
-            elif o == 2:
-                sum_ = sum_ * num[i]
-            elif o == 3 and sum_ >= 0:
-                sum_ = sum_ // num[i]
-            elif o==3 and sum_ < 0:
-                sum_ = -(-sum_//num[i])
-
-        if sum_ < min_sum:
-            min_sum = sum_
-        if sum_ > max_sum:
-            max_sum = sum_
-        sum_ = num[0]
+        oporder.add(order)
     else:
         for i in range(len(op)):
             if ch[i] == 0:
                 ch[i] = 1
-                dfs(v+1,order + [op[i]] )
+                dfs(v+1,order + str(op[i] ))
                 ch[i] = 0
 
 n = int(sys.stdin.readline().rstrip())
@@ -39,9 +19,32 @@ for i in range(4):
         [ op.append(i) for _ in range(math[i])]
 
 opnum = sum(math)
+oporder = set()
 ch = [0 for _ in range(opnum)]
-max_sum = 0
+max_sum = -21470000000
 min_sum = 21470000000
-dfs(0,[])
+dfs(0,'')
+
+cnt = 0
+for opo in oporder:
+    order = iter(map(int,list(opo)))
+    for i in range(1,n):
+        o = next(order)
+        if o == 0:
+            sum_ += num[i]
+        elif o == 1:
+            sum_ -= num[i]
+        elif o == 2:
+            sum_ = sum_ * num[i]
+        elif o == 3 and sum_ >= 0:
+            sum_ = sum_ // num[i]
+        elif o== 3 and sum_ < 0:
+            sum_ = -(-sum_//num[i])
+    if sum_ <= min_sum:
+        min_sum = sum_
+    if sum_ >= max_sum:
+        max_sum = sum_
+    sum_ = num[0]
+
 print(max_sum)
 print(min_sum)
